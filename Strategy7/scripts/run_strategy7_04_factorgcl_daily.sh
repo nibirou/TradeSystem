a04f638cb8ce3c98d6a8b350082e7bc12329ea18
nibirou <741420370@qq.com>
2026-04-03 07:37:14 +0000

    更新

--- Strategy7/scripts/run_strategy7_04_factorgcl_daily.sh
+++ Strategy7/scripts/run_strategy7_04_factorgcl_daily.sh
@@ -8,7 +8,7 @@ python3 ./Strategy7/run_strategy7.py \
   --data-root /workspace/Quant/data_baostock/stock_hist/hs300 \
   --hs300-list-path /workspace/Quant/data_baostock/metadata/stock_list_hs300.csv \
   --index-root /workspace/Quant/data_baostock/ak_index \
-  --train-start 2024-01-01 \
+  --train-start 2023-01-01 \
   --train-end 2024-12-31 \
   --test-start 2025-01-01 \
   --test-end 2025-12-31 \
@@ -36,7 +36,7 @@ python3 ./Strategy7/run_strategy7.py \
   --portfolio-model-type equal_weight \
   --execution-model-type ideal_fill \
   --execution-scheme vwap30_vwap30 \
-  --horizon 20 \
+  --horizon 5 \
   --top-k 10 \
   --long-threshold 0.50 \
   --fee-bps 1.5 \
